e44f3163fe6e1e38e0c937feb5a4b4312508725f
Evgeni Mateshev <asperov94@yandex.ru>
2020-08-17 18:53:09 +0300

    changed telegram.py

--- telegram.py
+++ telegram.py
@@ -1,6 +1,25 @@
 import json
 import requests
 import os
+from sys import argv
+
+#====================================
+#set variables
+#====================================
+
+url=str(os.environ[telegram_url])
+token=str(os.environ[telegram_token])
+chat_id=str(os.environ[telegram_chatid])
+#====================================
+#set messages
+#====================================
+text=str(argv[1])
 
 
-url=
+
+#====================================
+#send messages
+#====================================
+json={"chat_id": chat_id, "text": text}
+r=requests.post(url,json=json)
+
