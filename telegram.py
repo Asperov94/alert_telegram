23c84cf7314893d35212266cb70995053fa3f2ad
Evgeni Mateshev <asperov94@yandex.ru>
2020-08-19 12:23:03 +0300

    fix

--- telegram.py
+++ telegram.py
@@ -7,19 +7,35 @@ from sys import argv
 #set variables
 #====================================
 
-url=str(os.environ[telegram_url])
-token=str(os.environ[telegram_token])
-chat_id=str(os.environ[telegram_chatid])
+with open('env.sh') as file:
+    url = str(file.readline())
+    token = str(file.readline())
+    chat_id=str(file.readline())
+    command_api=str(file.readline())
+print (token)
+
+#url=str(os.environ[telegram_url])
+#token=str(os.environ[telegram_token])
+#chat_id=str(os.environ[telegram_chatid])
+#command_api=str(os.environ[telegram_command])
+
 #====================================
 #set messages
 #====================================
+
 text=str(argv[1])
 
+#====================================
+#send url split
+#====================================
 
-
+url=url+token+command_api
+url=url.replace("\n","")
+print(url)
 #====================================
 #send messages
 #====================================
+
 json={"chat_id": chat_id, "text": text}
 r=requests.post(url,json=json)
 
